ebc9d5415e16afb2696b03ca1bda7226754280a5
John Clark <johngclark36@gmail.com>
2017-01-20 20:18:08 -0500

    Prepare for final final heroku deployment

--- server.js
+++ server.js
@@ -30,4 +30,4 @@ app.use('/controllers', express.static(process.cwd() + '/app/controllers'))
 app.use('/public', express.static(process.cwd() + '/public'))
 routes(app, passport)
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || '8080')
